2682c24b55be09e7bc0fedc1fb195d4e5302c0db
Antoine EDOUARD <aedouard@e2r9p14.clusters.42paris.fr>
2022-08-04 01:05:08 +0200

    part skeleton

---
+++ includes/commands/Part.hpp
@@ -0,0 +1,16 @@
+#ifndef PART_HPP
+# define PART_HPP
+
+# include <iostream>
+# include <string>
+
+class Part
+{
+	public:
+		Part();
+		Part(Part const &src);
+		~Part();
+};
+
+
+#endif /* ************************************************************* PART_HPP */
